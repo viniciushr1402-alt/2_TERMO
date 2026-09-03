e40a0d862b66cd661a6c85003c2c6afc5b459c4c
Vinicius Henrique <viniciushr1402@gmail.com>
2026-09-03 15:03:22 -0300

    Corrigida estrutura das pastas

---
+++ PBE1/aula02/exercicio4.js
@@ -0,0 +1,10 @@
+const produto = {
+    nome: "Teclado Mrcanico",
+    preco: 150.00,
+    estoque:25,
+    emOferta: true
+
+};
+
+console.log(`Produto: ${produto.nome}`);
+console.log(`Preço: R$ ${produto.preco.toFixed(2)}`);
