91771281f87550ee244ed07efd547bc0779f977e
Vinicius Henrique <viniciushr1402@gmail.com>
2026-09-17 13:46:25 -0300

    LIMPANDO ARQUIVOS

--- COISAS INUTEIS/node_modules/BACKEND/ex1.js
+++
@@ -1,6 +0,0 @@
-const entrada = require('readline-sync')
-const num = entrada.questionInt("Tabuada de qual numero?")
-
-for (let i = 1; i <= 10; i+++)  {
-    console.log(`${num} x ${1} = ${num * 1}`);
-}
